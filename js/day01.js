e32afd2a985b21d916619983aedfda4a582254ec
Aaron Cook <aarongregorycook@gmail.com>
2021-11-30 23:23:15 -0600

    Day 1 solved

--- js/day01.js
+++ js/day01.js
@@ -1,11 +1,50 @@
 class Day01 {
-    constructor(input, star2) {
+    constructor(input, isstar2) {
         this.input = input;
-        this.star2 = star2;
+        this.isstar2 = isstar2;
     }
 
     solve() {
-        console.debug(`Solving star ${ this.star2 ? '2' : '1'}:`, this.input);
-        return 'Return later!';
+        this.parse();
+        if (this.isstar2) {
+            return this.star2();
+        } else {
+            return this.star1();
+        }
+    }
+
+    parse() {
+        this.depths = this.input.split('\n').map(line => {
+            return parseInt(line);
+        });
+    }
+
+    star1() {
+        var larger = 0;
+
+        for (var i = 1; i < this.depths.length; i++) {
+            if (this.depths[i] > this.depths[i-1]) {
+                larger++;
+            }
+        }
+
+        return String(larger);
+    }
+
+    star2() {
+        var larger = 0;
+        var prev = this.depths[0] + this.depths[1] + this.depths[2];
+
+        for (var i = 1; i < this.depths.length-2; i++) {
+            var cur = this.depths[i] + this.depths[i+1] + this.depths[i+2];
+
+            if (cur > prev) {
+                larger++;
+            }
+
+            prev = cur;
+        }
+
+        return String(larger);
     }
 }
