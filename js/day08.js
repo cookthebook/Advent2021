8c96a35242f2c00c12436126de026895794dda2d
Aaron Cook <aarongregorycook@gmail.com>
2021-12-08 23:32:15 -0600

    Day 8

--- js/day08.js
+++ js/day08.js
@@ -100,6 +100,165 @@ class day08 {
         return String(ret);
     }
 
+    _decode_pattern(patterns) {
+        var ret = new Array(10);
+        ret.fill(null);
+
+        /* just fill out unique segment counts */
+        patterns.forEach(pattern => {
+            var on_cnt = this._segments_on(pattern);
+
+            if (on_cnt === 2) {
+                ret[1] = pattern;
+            } else if (on_cnt === 3) {
+                ret[7] = pattern;
+            } else if (on_cnt === 4) {
+                ret[4] = pattern;
+            } else if (on_cnt === 7) {
+                ret[8] = pattern;
+            }
+        });
+
+        /* 5 segments on maps to 2, 3, or 5 */
+        for (var i = 0; i < patterns.length; i++) {
+            if (this._segments_on(patterns[i]) !== 5) {
+                continue;
+            }
+
+            /* 3 contains 1 */
+            if (ret[3] === null) {
+                var contains_1 = true;
+
+                for (var j = 0; j < Object.keys(ret[1]).length; j++) {
+                    var key = Object.keys(ret[1])[j];
+
+                    if (ret[1][key] === true && patterns[i][key] !== true) {
+                        contains_1 = false;
+                        break;
+                    }
+                }
+
+                if (contains_1) {
+                    ret[3] = patterns[i];
+                    continue;
+                }
+            }
+
+            /* 5 contains 1 segment of 1 and 3 segments of 4 */
+            if (ret[5] === null) {
+                var share_1 = 0;
+                var share_4 = 0;
+
+                for (var j = 0; j < Object.keys(ret[1]).length; j++) {
+                    var key = Object.keys(ret[1])[j];
+
+                    if (ret[1][key] === true && patterns[i][key] === true) {
+                        share_1++;
+                    }
+
+                    if (ret[4][key] === true && patterns[i][key] === true) {
+                        share_4++;
+                    }
+                }
+
+                if (share_1 === 1 && share_4 === 3) {
+                    ret[5] = patterns[i];
+                    continue;
+                }
+            }
+
+            /* 2 contains 1 segment of 1 and 2 segments of 4 */
+            if (ret[2] === null) {
+                var share_1 = 0;
+                var share_4 = 0;
+
+                for (var j = 0; j < Object.keys(ret[1]).length; j++) {
+                    var key = Object.keys(ret[1])[j];
+
+                    if (ret[1][key] === true && patterns[i][key] === true) {
+                        share_1++;
+                    }
+
+                    if (ret[4][key] === true && patterns[i][key] === true) {
+                        share_4++;
+                    }
+                }
+
+                if (share_1 === 1 && share_4 === 2) {
+                    ret[2] = patterns[i];
+                    continue;
+                }
+            }
+        }
+
+        /* 6 segments on maps to 0, 6, or 9 */
+        for (var i = 0; i < patterns.length; i++) {
+            if (this._segments_on(patterns[i]) !== 6) {
+                continue;
+            }
+
+            /* 0 contains 1, 9 contains 1 and 5, 6 does not contain 1 */
+            var contains_1 = true;
+            var contains_5 = true;
+
+            for (var j = 0; j < Object.keys(ret[1]).length; j++) {
+                var key = Object.keys(ret[1])[j];
+
+                if (ret[1][key] === true && patterns[i][key] !== true) {
+                    contains_1 = false;
+                }
+
+                if (ret[5][key] === true && patterns[i][key] !== true) {
+                    contains_5 = false;
+                }
+            }
+
+            if (contains_1 && contains_5) {
+                ret[9] = patterns[i];
+            } else if (contains_1) {
+                ret[0] = patterns[i];
+            } else {
+                ret[6] = patterns[i];
+            }
+        }
+
+        return ret;
+    }
+
+    _segs_are_equal(seg1, seg2) {
+        var ret = true;
+
+        for (var i = 0; i < Object.keys(seg1).length; i++) {
+            var k = Object.keys(seg1)[i];
+
+            if (seg1[k] !== seg2[k]) {
+                ret = false;
+                break;
+            }
+        }
+
+        return ret;
+    }
+
     star2() {
+        var ret = 0;
+
+        this.patterns.forEach((pattern, idx) => {
+            var decode = this._decode_pattern(pattern);
+            var digits = [0, 0, 0, 0];
+
+            this.digits[idx].forEach((d, i) => {
+                for (var j = 0; j < decode.length; j++) {
+                    if (this._segs_are_equal(decode[j], d)) {
+                        digits[i] = j;
+                        break;
+                    }
+                }
+            });
+
+            ret += digits[0]*(10**3) + digits[1]*(10**2) + digits[2]*10 + digits[3];
+        });
+
+        return String(ret);
     }
 }
