37a04cd42760c3ce1552f7daa18ac2d459f062d4
Aaron Cook <aarongregorycook@gmail.com>
2021-12-16 22:55:53 -0600

    Day 14 star 2

--- js/day14.js
+++ js/day14.js
@@ -1,4 +1,4 @@
-class day13 {
+class day14 {
     constructor(input, isstar2) {
         this.input = input;
         this.isstar2 = isstar2;
@@ -71,25 +71,63 @@ class day13 {
         return String(most-least);
     }
 
+    _insert_pairs2(counts, pair_counts, inserts) {
+        /* count new chars first */
+        Object.keys(pair_counts).forEach(pair => {
+            counts[inserts[pair]] += pair_counts[pair];
+        });
+
+        /* update pair counts */
+        let pair_counts_tmp = {};
+        Object.keys(pair_counts).forEach(pair => {
+            pair_counts_tmp[pair] = pair_counts[pair];
+            pair_counts[pair] = 0;
+        });
+        Object.keys(pair_counts).forEach(pair => {
+            let newp1 = pair[0] + inserts[pair];
+            let newp2 = inserts[pair] + pair[1];
+
+            pair_counts[newp1] += pair_counts_tmp[pair];
+            pair_counts[newp2] += pair_counts_tmp[pair];
+        });
+    }
+
     star2() {
         let counts = {};
+        let pair_counts = {};
         let least = -1;
         let most = -1;
 
+        /* setup individual letter count */
         for (let i = 0; i < this.polymer.length; i++) {
             counts[this.polymer[i]] = 0;
         }
-        this.inserts.forEach(insert => {
-            counts[insert] = 0;
-        })
+        Object.keys(this.inserts).forEach(insert => {
+            counts[this.inserts[insert]] = 0;
+        });
         for (let i = 0; i < this.polymer.length; i++) {
             counts[this.polymer[i]]++;
         }
 
-        for (let i = 0; i < 10; i++) {
+        /* setup pair count */
+        for (let i = 0; i < Object.keys(counts).length; i++) {
+            for (let j = 0; j < Object.keys(counts).length; j++) {
+                let key = Object.keys(counts)[i] + Object.keys(counts)[j];
+                pair_counts[key] = 0;
+            }
+        }
+        for (let i = 0; i < (this.polymer.length-1); i++) {
+            pair_counts[this.polymer.slice(i, i+2)]++;
         }
 
+        console.debug(this.polymer);
         console.debug(counts);
+        console.debug(pair_counts);
+
+        for (let i = 0; i < 40; i++) {
+            this._insert_pairs2(counts, pair_counts, this.inserts);
+        }
+
         Object.keys(counts).forEach(key => {
             if (least < 0 || counts[key] < least) {
                 least = counts[key];
